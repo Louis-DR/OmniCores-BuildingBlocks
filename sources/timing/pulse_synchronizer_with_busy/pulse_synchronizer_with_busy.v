6edd57ca32e1233d61c515a7e33001ad1a0b3125
Louis Duret-Robert <louisduret@gmail.com>
2024-05-11 17:22:48 +0200

    [New] Added pulse synchronizer variant with busy signal

--- sources/timing/pulse_synchronizer_with_busy/pulse_synchronizer_with_busy.v
+++ sources/timing/pulse_synchronizer_with_busy/pulse_synchronizer_with_busy.v
@@ -3,37 +3,40 @@
 // ║ Author:      Louis Duret-Robert - louisduret@gmail.com                    ║
 // ║ Website:     louis-dr.github.io                                           ║
 // ║ License:     MIT License                                                  ║
-// ║ File:        pulse_synchronizer.v                                         ║
+// ║ File:        pulse_synchronizer_with_busy.v                               ║
 // ╟───────────────────────────────────────────────────────────────────────────╢
 // ║ Description: Resynchronize a pulse signal lasting one clock cycle to a    ║
-// ║              different clock domain.                                      ║
+// ║              different clock domain, and notifies when it is busy.        ║
 // ║                                                                           ║
 // ║              If the default two stages of flip-flops are not enough to    ║
 // ║              prevent metastable outputs, three or more stages can be      ║
 // ║              used.                                                        ║
 // ║                                                                           ║
-// ║              Input pulses should be spaced out sufficiently to allow the  ║
-// ║              resynchronization to occur and to prevent metastability of   ║
-// ║              the input state flip-flop.                                   ║
+// ║              When a pulse is being resynchronized, the device is busy and ║
+// ║              no other input pulse can be received. The busy output signal ║
+// ║              can be used to prevent the upstream device from generating a ║
+// ║              pulse when the synchronizer is busy.                         ║
 // ║                                                                           ║
 // ╚═══════════════════════════════════════════════════════════════════════════╝
 
 
 
-module pulse_synchronizer #(
+module pulse_synchronizer_with_busy #(
   parameter STAGES = 2
 ) (
   input  source_clock,
   input  destination_clock,
   input  resetn,
   input  pulse_in,
-  output pulse_out
+  output pulse_out,
+  output busy
 );
 
 reg            state;
 reg [STAGES:0] stages;
+wire           feedback;
 
-wire state_resetn = resetn & ~stages[STAGES-1];
+wire state_resetn = resetn & ~feedback;
 
 always @(posedge source_clock or negedge state_resetn) begin
   if (!state_resetn) state <= 0;
@@ -52,5 +55,16 @@ always @(posedge destination_clock or negedge resetn) begin
 end
 
 assign pulse_out = stages[STAGES-1] & ~stages[STAGES];
+
+synchronizer #(
+  .STAGES   ( STAGES           )
+) feedback_synchronizer (
+  .clock    ( clock            ),
+  .resetn   ( resetn           ),
+  .data_in  ( stages[STAGES-1] ),
+  .data_out ( feedback         )
+);
+
+assign busy = state | feedback;
 
 endmodule
