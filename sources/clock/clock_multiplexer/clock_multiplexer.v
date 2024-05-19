1c59d9008d732b5bb8150d80bc19e8194831e712
Louis Duret-Robert <louisduret@gmail.com>
2024-05-19 16:33:45 +0200

    [Fix] Fix some issues in the clock multiplexer

--- sources/clock/clock_multiplexer/clock_multiplexer.v
+++ sources/clock/clock_multiplexer/clock_multiplexer.v
@@ -15,7 +15,7 @@
 
 
 module clock_multiplexer #(
-  STAGES = 2
+  parameter STAGES = 2
 ) (
   input  clock_0,
   input  clock_1,
@@ -24,19 +24,22 @@ module clock_multiplexer #(
   output clock_out
 );
 
+wire clock_0_inverted = ~clock_0;
+wire clock_1_inverted = ~clock_1;
+
 wire enable_clock_0;
 wire enable_clock_1;
 
 wire enable_clock_0_synchronized;
 wire enable_clock_1_synchronized;
 
-assign enable_clock_0 =  select & ~enable_clock_1_synchronized;
-assign enable_clock_1 = ~select & ~enable_clock_0_synchronized;
+assign enable_clock_0 = ~select & ~enable_clock_1_synchronized;
+assign enable_clock_1 =  select & ~enable_clock_0_synchronized;
 
 synchronizer #(
   .STAGES   ( STAGES                      )
 ) enable_clock_0_synchronizer (
-  .clock    ( clock_0                     ),
+  .clock    ( clock_0_inverted            ),
   .resetn   ( resetn                      ),
   .data_in  ( enable_clock_0              ),
   .data_out ( enable_clock_0_synchronized )
@@ -45,7 +48,7 @@ synchronizer #(
 synchronizer #(
   .STAGES   ( STAGES                      )
 ) enable_clock_1_synchronizer (
-  .clock    ( clock_1                     ),
+  .clock    ( clock_1_inverted            ),
   .resetn   ( resetn                      ),
   .data_in  ( enable_clock_1              ),
   .data_out ( enable_clock_1_synchronized )
