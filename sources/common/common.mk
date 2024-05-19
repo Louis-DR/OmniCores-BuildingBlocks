4e6da711fa1e0d50e2971e8f7f82001f6fe9af86
Louis Duret-Robert <louisduret@gmail.com>
2024-05-19 13:21:24 +0200

    [Verif] Create testbench for clock divider

--- sources/common/common.mk
+++ sources/common/common.mk
@@ -1,11 +1,13 @@
 RTL_SOURCES   ?= $(DESIGN_NAME).v $(DESIGN_NAME)_tb.sv
+RTL_SOURCES   := $(RTL_SOURCES) $(ADDITIONAL_RTL_SOURCES)
+RTL_INCLUDES  := $(addprefix -I ,$(RTL_INCLUDES))
 VVP_FILE      ?= $(DESIGN_NAME)_tb.vvp
 VCD_FILE      ?= $(DESIGN_NAME)_tb.vcd
 GTKW_FILE     ?= $(DESIGN_NAME)_tb.gtkw
 COMPILE_FLAGS ?=
 
 compile:
-	iverilog $(COMPILE_FLAGS) -o $(VVP_FILE) $(RTL_SOURCES) $(ADDITIONAL_RTL_SOURCES)
+	iverilog $(COMPILE_FLAGS) -o $(VVP_FILE) $(RTL_INCLUDES) $(RTL_SOURCES)
 
 simulate:
 	vvp $(VVP_FILE)
