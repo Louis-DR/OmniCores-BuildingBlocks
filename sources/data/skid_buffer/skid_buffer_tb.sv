043fd37072eb58ce5d455b5aeae2b4cacc80c15e
Louis Duret-Robert <louisduret@gmail.com>
2024-10-12 16:22:59 +0200

    [Verif] Update the testbench for the skid buffer after the change in ports + some improvements

--- sources/data/skid_buffer/skid_buffer_tb.sv
+++ sources/data/skid_buffer/skid_buffer_tb.sv
@@ -27,35 +27,36 @@ localparam integer THROUGHPUT_CHECK_DURATION          = 100;
 localparam integer RANDOM_CHECK_DURATION              = 100;
 localparam integer RANDOM_CHECK_INJECTION_PROBABILITY = 0.5;
 localparam integer RANDOM_CHECK_RECEPTION_PROBABILITY = 0.5;
+localparam integer RANDOM_CHECK_TIMEOUT               = 1000;
 
 // Device ports
 logic             clock;
 logic             resetn;
-logic [WIDTH-1:0] upstream_data;
-logic             upstream_valid;
-logic             upstream_ready;
-logic [WIDTH-1:0] downstream_data;
-logic             downstream_valid;
-logic             downstream_ready;
+logic             write_enable;
+logic [WIDTH-1:0] write_data;
+logic             full;
+logic             read_enable;
+logic [WIDTH-1:0] read_data;
+logic             empty;
 
 // Test variables
 integer data_expected[$];
 integer pop_trash;
-integer injection_count;
-bool    injecting;
+integer transfer_count;
+integer timeout_countdown;
 
 // Device under test
 skid_buffer #(
   .WIDTH ( WIDTH )
 ) skid_buffer_dut (
-  .clock            ( clock            ),
-  .resetn           ( resetn           ),
-  .upstream_data    ( upstream_data    ),
-  .upstream_valid   ( upstream_valid   ),
-  .upstream_ready   ( upstream_ready   ),
-  .downstream_data  ( downstream_data  ),
-  .downstream_valid ( downstream_valid ),
-  .downstream_ready ( downstream_ready )
+  .clock        ( clock        ),
+  .resetn       ( resetn       ),
+  .write_enable ( write_enable ),
+  .write_data   ( write_data   ),
+  .full         ( full         ),
+  .read_enable  ( read_enable  ),
+  .read_data    ( read_data    ),
+  .empty        ( empty        )
 );
 
 // Source clock generation
@@ -73,9 +74,9 @@ initial begin
   $dumpvars(0,skid_buffer_tb);
 
   // Initialization
-  upstream_data    = 0;
-  upstream_valid   = 0;
-  downstream_ready = 0;
+  write_data   = 0;
+  write_enable = 0;
+  read_enable  = 0;
 
   // Reset
   resetn = 0;
@@ -83,107 +84,143 @@ initial begin
   resetn = 1;
   #(CLOCK_PERIOD);
 
-  // Check 1 : Saturating
-  downstream_ready = 0;
-  $display("CHECK 1 : Saturating.");
-  if (downstream_valid != 0) $error("[%0tns] Downstream valid is asserted after reset with data '%0h'. It shouldn't be sending data.", $time, downstream_data);
-  if (upstream_ready   != 1) $error("[%0tns] Upstream ready is deasserted after reset. It should be accepting tranfers.", $time);
-  @(negedge clock); upstream_valid = 1; upstream_data = 8'b10101010; data_expected.push_back(upstream_data);
-  @(negedge clock); upstream_valid = 0; upstream_data = 0;
-  if (downstream_valid != 1) $error("[%0tns] Downstream valid is deasserted after the first transfer. It should be sending the first transfer.", $time);
-  if (upstream_ready   != 1) $error("[%0tns] Upstream ready is deasserted after the first transfer. It should be accepting tranfers.", $time);
-  @(negedge clock); upstream_valid = 1; upstream_data = 8'b01010101; data_expected.push_back(upstream_data);
-  @(negedge clock); upstream_valid = 0; upstream_data = 0;
-  if (downstream_valid != 1) $error("[%0tns] Downstream valid is deasserted after the second transfer. It should be sending the first transfer.", $time);
-  if (upstream_ready   != 0) $error("[%0tns] Upstream ready is asserted after the second transfer. It shouldn't be accepting transfers.", $time);
+  // Check 1 : Writing to full
+  @(negedge clock);
+  read_enable = 0;
+  $display("CHECK 1 : Writing to full.");
+  // Initial state
+  if (empty != 1) $error("[%0tns] Empty flag is deasserted after reset with data '%0h'. The buffer should be empty.", $time, read_data);
+  if (full  != 0) $error("[%0tns] Full flag is asserted after reset. The buffer should be empty.", $time);
+  // First write
+  @(negedge clock); write_enable = 1; write_data = 8'b10101010; data_expected.push_back(write_data);
+  @(negedge clock); write_enable = 0; write_data = 0;
+  if (empty != 0) $error("[%0tns] Empty flag is asserted after the first write. The buffer should contain the first transfer.", $time);
+  if (full  != 0) $error("[%0tns] Full flag is asserted after the first write. The buffer should still have one free slot.", $time);
+  // Second write
+  @(negedge clock); write_enable = 1; write_data = 8'b01010101; data_expected.push_back(write_data);
+  @(negedge clock); write_enable = 0; write_data = 0;
+  if (empty != 0) $error("[%0tns] Empty flag is asserted after the second write. The buffer should contain the first transfer.", $time);
+  if (full  != 1) $error("[%0tns] Full flag is deasserted after the second write. The buffer should be full.", $time);
 
-  // Check 2 : Clearing
-  $display("CHECK 2 : Clearing.");
-  @(negedge clock); downstream_ready = 1;
-  if (downstream_data != data_expected[0]) $error("[%0tns] First data returned '%0h' is not as expected '%0h'.", $time, downstream_data, data_expected[0]);
-  @(negedge clock); downstream_ready = 0; pop_trash = data_expected.pop_front();
-  if (downstream_valid != 1) $error("[%0tns] Downstream valid is deasserted after the first reception. It should be sending the second transfer.", $time);
-  if (upstream_ready   != 1) $error("[%0tns] Upstream ready is deasserted after the first reception. It should be accepting tranfers.", $time);
-  @(negedge clock); downstream_ready = 1;
-  if (downstream_data != data_expected[0]) $error("[%0tns] Second data returned '%0h' is not as expected '%0h'.", $time, downstream_data, data_expected[0]);
-  @(negedge clock); downstream_ready = 0; pop_trash = data_expected.pop_front();
-  if (downstream_valid != 0) $error("[%0tns] Downstream valid is asserted after the second reception with data '%0h'. It shouldn't be sending data.", $time, downstream_data);
-  if (upstream_ready   != 1) $error("[%0tns] Upstream ready is deasserted after the second reception. It should be accepting tranfers.", $time);
+  repeat(10) @(posedge clock);
+
+  // Check 2 : Reading to empty
+  $display("CHECK 2 : Reading to empty.");
+  // First read
+  @(negedge clock); read_enable = 1;
+  if (read_data != data_expected[0]) $error("[%0tns] First data returned '%0h' is not as expected '%0h'.", $time, read_data, data_expected[0]);
+  @(negedge clock); read_enable = 0; pop_trash = data_expected.pop_front();
+  if (empty != 0) $error("[%0tns] Empty flag is asserted after the first read. The buffer should contain the second transfer.", $time);
+  if (full  != 0) $error("[%0tns] Full flag is asserted after the first read. The buffer should have one free slot.", $time);
+  // Second read
+  @(negedge clock); read_enable = 1;
+  if (read_data != data_expected[0]) $error("[%0tns] Second data returned '%0h' is not as expected '%0h'.", $time, read_data, data_expected[0]);
+  @(negedge clock); read_enable = 0; pop_trash = data_expected.pop_front();
+  if (empty != 1) $error("[%0tns] Empty flag is deasserted after the second read with data '%0h'. The buffer should be empty.", $time, read_data);
+  if (full  != 0) $error("[%0tns] Full flag is asserted after the second read. The buffer should be empty.", $time);
+
+  repeat(10) @(posedge clock);
 
   // Check 3 : Back-to-back transfers for full throughput
   $display("CHECK 3 : Back-to-back transfers for full throughput.");
-  upstream_valid   = 1;
-  upstream_data    = 0;
-  downstream_ready = 1;
+  @(negedge clock);
+  // Write
+  write_enable = 1;
+  write_data   = 0;
   for (integer iteration=1 ; iteration<THROUGHPUT_CHECK_DURATION ; iteration++) begin
-    data_expected.push_back(upstream_data);
+    data_expected.push_back(write_data);
     @(negedge clock);
-    if (downstream_valid != 1)                $error("[%0tns] Downstream valid is deasserted. It should be sending transfers.", $time);
-    if (upstream_ready   != 1)                $error("[%0tns] Upstream ready is deasserted. It should be accepting tranfers.", $time);
-    if (downstream_data  != data_expected[0]) $error("[%0tns] Data returned '%0h' is not as expected '%0h'.", $time, downstream_data, data_expected[0]);
-    pop_trash = data_expected.pop_front();
-    upstream_data = upstream_data+1;
+    if (empty != 0) $error("[%0tns] Empty flag is asserted. The buffer should be sending transfers.", $time);
+    if (full  != 0) $error("[%0tns] Full flag is asserted. The buffer should be accepting tranfers.", $time);
+    // Read when not empty
+    if (!empty) begin
+      read_enable = 1;
+      if (read_data != data_expected[0]) $error("[%0tns] Data returned '%0h' is not as expected '%0h'.", $time, read_data, data_expected[0]);
+      pop_trash = data_expected.pop_front();
+    end
+    // Increment write data
+    write_data = write_data+1;
   end
-  upstream_valid = 0;
-  upstream_data  = 0;
+  write_enable = 0;
+  write_data   = 0;
+  // Last read
+  @(negedge clock);
+  read_enable = 0;
+  // Final state
+  if (empty != 1) $error("[%0tns] Empty flag is deasserted after check 3. The buffer should be empty.", $time);
+  if (full  != 0) $error("[%0tns] Full flag is asserted after check 3. The buffer should be empty.", $time);
 
   repeat(10) @(posedge clock);
 
   // Check 4 : Random stimulus
   $display("CHECK 4 : Random stimulus.");
-  downstream_ready = 0;
-  injection_count  = 0;
-  injecting        = 0;
   @(negedge clock);
+  transfer_count    = 0;
+  timeout_countdown = RANDOM_CHECK_TIMEOUT;
   fork
     // Injection
     begin
       forever begin
+        // Stimulus
         @(negedge clock);
-        if (!injecting) begin
-          if ($random < RANDOM_CHECK_INJECTION_PROBABILITY) begin
-            injecting      = 1;
-            upstream_valid = 1;
-            upstream_data  = $urandom_range(WIDTH_POW2);
-          end else begin
-            upstream_valid = 0;
-            upstream_data  = 0;
-          end
+        if (!full && $random < RANDOM_CHECK_INJECTION_PROBABILITY) begin
+          write_enable = 1;
+          write_data   = $urandom_range(WIDTH_POW2);
+        end else begin
+          write_enable = 0;
+          write_data   = 0;
         end
+        // Check
         @(posedge clock);
-        if (injecting) begin
-          if (upstream_ready) begin
-            data_expected.push_back(upstream_data);
-            injection_count++;
-            injecting = 0;
-          end
+        if (write_enable) begin
+          data_expected.push_back(write_data);
+          transfer_count++;
         end
       end
     end
     // Reception
     begin
       forever begin
+        // Stimulus
         @(negedge clock);
-        if ($random < RANDOM_CHECK_RECEPTION_PROBABILITY) begin
-          downstream_ready = 1;
+        if (!empty && $random < RANDOM_CHECK_RECEPTION_PROBABILITY) begin
+          read_enable = 1;
         end else begin
-          downstream_ready = 0;
+          read_enable = 0;
         end
+        // Check
         @(posedge clock);
-        if (downstream_ready && downstream_valid) begin
-          if (downstream_data != data_expected[0]) $error("[%0tns] Data returned '%0h' is not as expected '%0h'.", $time, downstream_data, data_expected[0]);
+        if (read_enable) begin
+          if (read_data != data_expected[0]) $error("[%0tns] Data returned '%0h' is not as expected '%0h'.", $time, read_data, data_expected[0]);
           pop_trash = data_expected.pop_front();
         end
       end
     end
     // Stop condition
     begin
-      while (injection_count < RANDOM_CHECK_DURATION) begin
+      // Transfer count
+      while (transfer_count < RANDOM_CHECK_DURATION) begin
         @(negedge clock);
+      end
+      // Read until empty
+      while (!empty) begin
+        @(negedge clock);
+      end
+    end
+    // Timeout
+    begin
+      while (timeout_countdown > 0) begin
+        @(negedge clock);
+        timeout_countdown--;
       end
     end
   join_any
   disable fork;
+  // Final state
+  if (empty != 1) $error("[%0tns] Empty flag is deasserted after check 4. The buffer should be empty.", $time);
+  if (full  != 0) $error("[%0tns] Full flag is asserted after check 4. The buffer should be empty.", $time);
+
+  repeat(10) @(posedge clock);
 
   // End of test
   $finish;
