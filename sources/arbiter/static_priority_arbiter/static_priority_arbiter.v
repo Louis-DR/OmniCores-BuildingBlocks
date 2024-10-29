74f3be857aeca31916943832ae31309fbf5ba9e7
Louis Duret-Robert <louisduret@gmail.com>
2024-10-30 00:27:59 +0100

    [New] Added static priority arbiter

---
+++ sources/arbiter/static_priority_arbiter/static_priority_arbiter.v
@@ -0,0 +1,32 @@
+// ╔═══════════════════════════════════════════════════════════════════════════╗
+// ║ Project:     OmniCores-BuildingBlocks                                     ║
+// ║ Author:      Louis Duret-Robert - louisduret@gmail.com                    ║
+// ║ Website:     louis-dr.github.io                                           ║
+// ║ License:     MIT License                                                  ║
+// ║ File:        static_priority_arbiter.v                                    ║
+// ╟───────────────────────────────────────────────────────────────────────────╢
+// ║ Description: Arbiters between different request channels. The grant is    ║
+// ║              given to the first ready request channel (the least          ║
+// ║              significant one bit).                                        ║
+// ║                                                                           ║
+// ╚═══════════════════════════════════════════════════════════════════════════╝
+
+
+
+module static_priority_arbiter #(
+  parameter SIZE = 4
+) (
+  input             clock,
+  input             resetn,
+  input  [SIZE-1:0] requests,
+  output [SIZE-1:0] grant
+);
+
+assign grant[0] = requests[0];
+generate
+  for (genvar bit_index = 1; bit_index < SIZE; bit_index++) begin : gen_bits
+    assign grant[bit_index] = ~grant[bit_index-1] & requests[bit_index];
+  end
+endgenerate
+
+endmodule
