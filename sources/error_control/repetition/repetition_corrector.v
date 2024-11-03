8ddbb1ea2113595a57d87e4fb0499f781bf17752
Louis Duret-Robert <louisduret@gmail.com>
2024-11-03 23:42:45 +0100

    [Fix] Fixes to the repetition corrector : syntax + undriven error status output

--- sources/error_control/repetition/repetition_corrector.v
+++ sources/error_control/repetition/repetition_corrector.v
@@ -23,22 +23,27 @@ module repetition_corrector #(
 
 wire [REPETITION*DATA_WIDTH-1:0] block = {data, code};
 
+// Group bits by groups of repetition
 wire [REPETITION-1:0] grouped_bits [DATA_WIDTH-1:0];
-
 generate
-  for (genvar repetition_index = 0; repetition_index < REPETITION; repetition_index = repetition_index++) begin : gen_repetitions
+  for (genvar repetition_index = 0; repetition_index < REPETITION; repetition_index++) begin : gen_repetitions
     for (genvar bit_index = 0; bit_index < DATA_WIDTH; bit_index++) begin : gen_bits
       assign grouped_bits[bit_index][repetition_index] = block[repetition_index*DATA_WIDTH + bit_index];
     end
   end
 endgenerate
 
+// Detect repetition errors
+wire [DATA_WIDTH-1:0] error_position;
 generate
   for (genvar bit_index = 0; bit_index < DATA_WIDTH; bit_index++) begin : gen_bits
     wire all_ones  =  & grouped_bits[bit_index];
     wire all_zeros = ~| grouped_bits[bit_index];
+    assign error_position[bit_index] = ~(all_ones | all_zeros);
     assign corrected_data[bit_index] = ($countones(grouped_bits[bit_index]) > (REPETITION / 2)) ? 1'b1 : 1'b0;
   end
 endgenerate
+
+assign error = |error_position;
 
 endmodule
