bbc308a048df4e57c9c5d07adf9d82e773be9ef7
Louis Duret-Robert <louisduret@gmail.com>
2025-05-10 13:28:42 +0200

    [Change] Make the padding value of the barrel shifter dynamic instead of a static parameter

--- sources/operations/barrel_shifter_left/barrel_shifter_left.v
+++ sources/operations/barrel_shifter_left/barrel_shifter_left.v
@@ -17,15 +17,15 @@
 
 module barrel_shifter_left #(
   parameter WIDTH      = 8,
-  parameter WIDTH_LOG2 = `CLOG2(WIDTH),
-  parameter PAD_VALUE  = 1'b0
+  parameter WIDTH_LOG2 = `CLOG2(WIDTH)
 ) (
   input      [WIDTH-1:0] data_in,
   input [WIDTH_LOG2-1:0] shift,
+  input                  pad_value,
   output     [WIDTH-1:0] data_out
 );
 
-wire [2*WIDTH-1:0] data_in_extended = {data_in, {WIDTH{PAD_VALUE}}};
+wire [2*WIDTH-1:0] data_in_extended = {data_in, {WIDTH{pad_value}}};
 assign data_out = data_in_extended[WIDTH - shift +: WIDTH];
 
 endmodule
--- sources/operations/barrel_shifter_right/barrel_shifter_right.v
+++ sources/operations/barrel_shifter_right/barrel_shifter_right.v
@@ -17,15 +17,15 @@
 
 module barrel_shifter_right #(
   parameter WIDTH      = 8,
-  parameter WIDTH_LOG2 = `CLOG2(WIDTH),
-  parameter PAD_VALUE  = 1'b0
+  parameter WIDTH_LOG2 = `CLOG2(WIDTH)
 ) (
   input      [WIDTH-1:0] data_in,
   input [WIDTH_LOG2-1:0] shift,
+  input                  pad_value,
   output     [WIDTH-1:0] data_out
 );
 
-wire [2*WIDTH-1:0] data_in_extended = {{WIDTH{PAD_VALUE}}, data_in};
+wire [2*WIDTH-1:0] data_in_extended = {{WIDTH{pad_value}}, data_in};
 assign data_out = data_in_extended[shift +: WIDTH];
 
 endmodule
