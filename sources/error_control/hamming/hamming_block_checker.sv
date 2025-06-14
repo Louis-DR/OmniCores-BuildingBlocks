fea1d3a9516a0d92d33c282507bb17fc716db04c
Louis Duret-Robert <louisduret@gmail.com>
2025-06-14 14:53:00 +0200

    [New] Added Hamming block checker

---
+++ sources/error_control/hamming/hamming_block_checker.sv
@@ -0,0 +1,55 @@
+// ╔═══════════════════════════════════════════════════════════════════════════╗
+// ║ Project:     OmniCores-BuildingBlocks                                     ║
+// ║ Author:      Louis Duret-Robert - louisduret@gmail.com                    ║
+// ║ Website:     louis-dr.github.io                                           ║
+// ║ License:     MIT License                                                  ║
+// ║ File:        hamming_block_checker.sv                                     ║
+// ╟───────────────────────────────────────────────────────────────────────────╢
+// ║ Description: Check a Hamming block for errors using syndrome calculation. ║
+// ║                                                                           ║
+// ╚═══════════════════════════════════════════════════════════════════════════╝
+
+
+
+`include "hamming.svh"
+
+
+
+module hamming_block_checker #(
+  parameter  BLOCK_WIDTH  = 15,
+  localparam DATA_WIDTH   = `GET_HAMMING_DATA_WIDTH_FROM_BLOCK_WIDTH(BLOCK_WIDTH),
+  localparam PARITY_WIDTH = `GET_HAMMING_PARITY_WIDTH_FROM_BLOCK_WIDTH(BLOCK_WIDTH)
+) (
+  input [BLOCK_WIDTH-1:0] block,
+  output                  error
+);
+
+logic   [DATA_WIDTH-1:0] data;
+logic [PARITY_WIDTH-1:0] received_code;
+
+hamming_block_extractor #(
+  .BLOCK_WIDTH ( BLOCK_WIDTH )
+) extractor (
+  .block ( block        ),
+  .data  ( data         ),
+  .code  ( received_code )
+);
+
+logic [PARITY_WIDTH-1:0] expected_code;
+
+hamming_encoder #(
+  .DATA_WIDTH ( DATA_WIDTH )
+) encoder (
+  .data  ( data          ),
+  .code  ( expected_code ),
+  .block (               )
+);
+
+// Calculate syndrome
+logic [PARITY_WIDTH-1:0] syndrome;
+assign syndrome = received_code ^ expected_code;
+
+// Error detected if syndrome is non-zero
+assign error = |syndrome;
+
+endmodule
