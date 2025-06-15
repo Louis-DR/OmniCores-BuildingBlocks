8d4c9356b384c1a066bd9e141857d3959caf7ff9
Louis Duret-Robert <louisduret@gmail.com>
2025-06-15 10:25:23 +0200

    [Fix] Finish the extended Hamming encoder block

--- sources/error_control/extended_hamming/extended_hamming_encoder.sv
+++ sources/error_control/extended_hamming/extended_hamming_encoder.sv
@@ -12,50 +12,42 @@
 
 
 
+`include "extended_hamming.svh"
+
+
+
 module extended_hamming_encoder #(
   parameter  DATA_WIDTH   = 8,
-  localparam PARITY_WIDTH = (DATA_WIDTH <=     1) ?  3 : // Hamming(  4,  1)
-                            (DATA_WIDTH <=     4) ?  4 : // Hamming(  8,  4)
-                            (DATA_WIDTH <=    11) ?  5 : // Hamming( 16, 11)
-                            (DATA_WIDTH <=    26) ?  6 : // Hamming( 32, 26)
-                            (DATA_WIDTH <=    57) ?  7 : // Hamming( 64, 57)
-                            (DATA_WIDTH <=   120) ?  8 : // Hamming(128,120)
-                            (DATA_WIDTH <=   247) ?  9 : // Hamming(256,247)
-                            (DATA_WIDTH <=   502) ? 10 :
-                            (DATA_WIDTH <=  1013) ? 11 :
-                            (DATA_WIDTH <=  2036) ? 12 :
-                            (DATA_WIDTH <=  4083) ? 13 :
-                            (DATA_WIDTH <=  8178) ? 14 :
-                            (DATA_WIDTH <= 16369) ? 15 :
-                            (DATA_WIDTH <= 32752) ? 16 :
-                            (DATA_WIDTH <= 65519) ? 17 : -1,
-  localparam BLOCK_WIDTH = DATA_WIDTH+PARITY_WIDTH
+  localparam PARITY_WIDTH = `GET_EXTENDED_HAMMING_PARITY_WIDTH(DATA_WIDTH),
+  localparam BLOCK_WIDTH  = DATA_WIDTH + PARITY_WIDTH
 ) (
   input    [DATA_WIDTH-1:0] data,
   output [PARITY_WIDTH-1:0] code,
   output  [BLOCK_WIDTH-1:0] block
 );
 
-// First generate the Hamming parity bits
-wire [BLOCK_WIDTH-2:0] hamming_block;
+// Generate the standard Hamming code
+wire [PARITY_WIDTH-2:0] hamming_code;
+wire  [BLOCK_WIDTH-2:0] hamming_block;
 hamming_encoder #(
   .DATA_WIDTH ( DATA_WIDTH )
 ) hamming_encoder (
   .data  ( data          ),
-  .code  (               ),
+  .code  ( hamming_code  ),
   .block ( hamming_block )
 );
 
-// Second generate the extra parity bit
+// Generate the extra parity bit
 wire extra_parity;
 parity_encoder #(
   .DATA_WIDTH ( BLOCK_WIDTH-1 )
 ) parity_encoder (
-  .data   ( hamming_block ),
-  .code   ( extra_parity  )
+  .data ( hamming_block ),
+  .code ( extra_parity  )
 );
 
-// Concatenate the block
+// Concatenate the code and the block
+assign code  = {hamming_code,  extra_parity};
 assign block = {hamming_block, extra_parity};
 
 endmodule
