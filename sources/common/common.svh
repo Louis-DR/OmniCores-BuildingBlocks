3487541d8840fda0ed3886ee40e6789878152ded
Louis Duret-Robert <louisduret@gmail.com>
2025-06-16 22:45:31 +0200

    [Verif] Removing sample N out of range M function from common SVH since it is not supported by Icarus Verilog

--- sources/common/common.svh
+++ sources/common/common.svh
@@ -12,32 +12,4 @@ typedef bit bool;
 localparam bool true  = 1'b1;
 localparam bool false = 1'b0;
 
-// Function to sample N elements from range [0, M-1] without replacement
-function automatic integer random_range_sample_2 (input integer M);
-  integer sample[2];
-  integer range[];
-  integer temporary;
-
-  // Allocate and initialize range array with range [0, M-1]
-  range = new[M];
-  for (int range_index = 0; range_index < M; range_index++) begin
-    range[range_index] = range_index;
-  end
-
-  // Partial Fisher-Yates shuffle - only shuffle first N elements
-  for (int shuffle_index = 0; shuffle_index < 2; shuffle_index++) begin
-    int random_index = $urandom_range(shuffle_index, M-1);
-
-    // Swap range[shuffle_index] with range[random_index]
-    temporary = range[shuffle_index];
-    range[shuffle_index] = range[random_index];
-    range[random_index] = temporary;
-
-    // Store shuffled element in sample
-    sample[shuffle_index] = range[shuffle_index];
-  end
-
-  return sample;
-endfunction
-
 `endif
