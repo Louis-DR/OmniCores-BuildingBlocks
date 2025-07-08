6aa1b2aa2ae94ac18a2ec66f3f3b2ae757d0c7d0
Louis Duret-Robert <louisduret@gmail.com>
2025-07-08 12:57:40 +0200

    [Verif] Common macro to count set bits in value

---
+++ sources/common/count_ones.svh
@@ -0,0 +1,15 @@
+`ifndef __OMNICORES_COUNT_ONES__
+`define __OMNICORES_COUNT_ONES__
+
+// Macro to count the number of ones (set bits) in a value
+`define COUNT_ONES(width, value, result) \
+  begin \
+    result = 0; \
+    for (integer bit_index_temp = 0; bit_index_temp < width; bit_index_temp++) begin \
+      if (value[bit_index_temp]) begin \
+        result++; \
+      end \
+    end \
+  end
+
+`endif
