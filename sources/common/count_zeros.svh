2a2e7d7bc337e14124fb5056602f3471426f8a82
Louis Duret-Robert <louisduret@gmail.com>
2025-07-08 12:57:57 +0200

    [Verif] Common macro to count zero bits in value

---
+++ sources/common/count_zeros.svh
@@ -0,0 +1,15 @@
+`ifndef __OMNICORES_COUNT_ZEROS__
+`define __OMNICORES_COUNT_ZEROS__
+
+// Macro to count the number of zeros (clear bits) in a value
+`define COUNT_ZEROS(width, value, result) \
+  begin \
+    result = 0; \
+    for (integer bit_index_temp = 0; bit_index_temp < width; bit_index_temp++) begin \
+      if (!value[bit_index_temp]) begin \
+        result++; \
+      end \
+    end \
+  end
+
+`endif
