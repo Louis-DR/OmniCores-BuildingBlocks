e5cdff803d7a1373506f5fe7b8abb4afe5ace17e
Louis Duret-Robert <louisduret@gmail.com>
2025-09-06 20:20:18 +0200

    [Change] Rename the dual-port RAMs to simple dual-port RAMs (SDP)

--- sources/memory/asynchronous_simple_dual_port_ram/asynchronous_simple_dual_port_ram.v
+++ sources/memory/asynchronous_simple_dual_port_ram/asynchronous_simple_dual_port_ram.v
@@ -3,9 +3,10 @@
 // ║ Author:      Louis Duret-Robert - louisduret@gmail.com                    ║
 // ║ Website:     louis-dr.github.io                                           ║
 // ║ License:     MIT License                                                  ║
-// ║ File:        dual_port_ram.v                                              ║
+// ║ File:        asynchronous_simple_dual_port_ram.v                          ║
 // ╟───────────────────────────────────────────────────────────────────────────╢
-// ║ Description: Random access memory with separate ports for read and write. ║
+// ║ Description: Random access memory with separate asynchronous ports for    ║
+// ║              read and write.                                              ║
 // ║                                                                           ║
 // ╚═══════════════════════════════════════════════════════════════════════════╝
 
@@ -15,33 +16,32 @@
 
 
 
-module dual_port_ram #(
+module asynchronous_simple_dual_port_ram #(
   parameter WIDTH = 8,
   parameter DEPTH = 16,
   parameter ADDRESS_WIDTH = `CLOG2(DEPTH)
 ) (
-  input                      clock,
-  input                      resetn,
   // Write interface
+  input                      write_clock,
+  input                      write_resetn,
   input                      write_enable,
   input  [ADDRESS_WIDTH-1:0] write_address,
   input          [WIDTH-1:0] write_data,
   // Read interface
+  input                      read_clock,
+  input                      read_resetn,
   input                      read_enable,
   input  [ADDRESS_WIDTH-1:0] read_address,
-  output         [WIDTH-1:0] read_data
+  output reg     [WIDTH-1:0] read_data
 );
 
 // Memory array
 reg [WIDTH-1:0] memory [DEPTH-1:0];
 
-// Read logic
-assign read_data = read_enable ? memory[read_address] : 0;
-
 integer depth_index;
-always @(posedge clock or negedge resetn) begin
+always @(posedge write_clock or negedge write_resetn) begin
   // Reset
-  if (!resetn) begin
+  if (!write_resetn) begin
     for (depth_index = 0; depth_index < DEPTH; depth_index = depth_index+1) begin
       memory[depth_index] <= 0;
     end
@@ -49,6 +49,17 @@ always @(posedge clock or negedge resetn) begin
   // Write
   else if (write_enable) begin
     memory[write_address] <= write_data;
+  end
+end
+
+always @(posedge read_clock or negedge read_resetn) begin
+  // Reset
+  if (!read_resetn) begin
+    read_data <= 0;
+  end
+  // Read
+  else if (read_enable) begin
+    read_data <= memory[read_address];
   end
 end
 
--- sources/memory/simple_dual_port_ram/simple_dual_port_ram.v
+++ sources/memory/simple_dual_port_ram/simple_dual_port_ram.v
@@ -3,10 +3,9 @@
 // ║ Author:      Louis Duret-Robert - louisduret@gmail.com                    ║
 // ║ Website:     louis-dr.github.io                                           ║
 // ║ License:     MIT License                                                  ║
-// ║ File:        asynchronous_dual_port_ram.v                                 ║
+// ║ File:        simple_dual_port_ram.v                                       ║
 // ╟───────────────────────────────────────────────────────────────────────────╢
-// ║ Description: Random access memory with separate asynchronous ports for    ║
-// ║              read and write.                                              ║
+// ║ Description: Random access memory with separate ports for read and write. ║
 // ║                                                                           ║
 // ╚═══════════════════════════════════════════════════════════════════════════╝
 
@@ -16,32 +15,33 @@
 
 
 
-module asynchronous_dual_port_ram #(
+module simple_dual_port_ram #(
   parameter WIDTH = 8,
   parameter DEPTH = 16,
   parameter ADDRESS_WIDTH = `CLOG2(DEPTH)
 ) (
+  input                      clock,
+  input                      resetn,
   // Write interface
-  input                      write_clock,
-  input                      write_resetn,
   input                      write_enable,
   input  [ADDRESS_WIDTH-1:0] write_address,
   input          [WIDTH-1:0] write_data,
   // Read interface
-  input                      read_clock,
-  input                      read_resetn,
   input                      read_enable,
   input  [ADDRESS_WIDTH-1:0] read_address,
-  output reg     [WIDTH-1:0] read_data
+  output         [WIDTH-1:0] read_data
 );
 
 // Memory array
 reg [WIDTH-1:0] memory [DEPTH-1:0];
 
+// Read logic
+assign read_data = read_enable ? memory[read_address] : 0;
+
 integer depth_index;
-always @(posedge write_clock or negedge write_resetn) begin
+always @(posedge clock or negedge resetn) begin
   // Reset
-  if (!write_resetn) begin
+  if (!resetn) begin
     for (depth_index = 0; depth_index < DEPTH; depth_index = depth_index+1) begin
       memory[depth_index] <= 0;
     end
@@ -49,17 +49,6 @@ always @(posedge write_clock or negedge write_resetn) begin
   // Write
   else if (write_enable) begin
     memory[write_address] <= write_data;
-  end
-end
-
-always @(posedge read_clock or negedge read_resetn) begin
-  // Reset
-  if (!read_resetn) begin
-    read_data <= 0;
-  end
-  // Read
-  else if (read_enable) begin
-    read_data <= memory[read_address];
   end
 end
 
