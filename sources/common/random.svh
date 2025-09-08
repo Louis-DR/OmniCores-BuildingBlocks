71a89a7e49c3782b272970856f84ca793f79dbb5
Louis Duret-Robert <louisduret@gmail.com>
2025-09-08 10:57:56 +0200

    [Verif] Improve the random helper functins

--- sources/common/random.svh
+++ sources/common/random.svh
@@ -12,4 +12,28 @@ function bool random_boolean(input real probability);
   return random_ratio() < probability;
 endfunction
 
+function automatic int random_in_mask(input longint unsigned mask);
+  int unsigned number_high_bits;
+  int unsigned random_target;
+  int unsigned high_bit_count;
+  // Count high bits in mask
+  number_high_bits = $countones(mask);
+  // If none, return -1
+  if (number_high_bits == 0) return -1;
+  // Select random index within high bits
+  random_target = $urandom_range(number_high_bits - 1);
+  // Convert to mask index
+  high_bit_count = 0;
+  foreach (mask[index]) begin
+    if (mask[index]) begin
+      if (high_bit_count == random_target) begin
+        return index;
+      end
+      high_bit_count++;
+    end
+  end
+  // Fallback
+  return -1;
+endfunction
+
 `endif
