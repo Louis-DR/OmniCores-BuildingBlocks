24006d9a35e9d0a74f5e0652c1dda19655704994
Louis Duret-Robert <louisduret@gmail.com>
2025-09-08 10:57:00 +0200

    [Verif] Improve the tag directory testbench with random and sequential checks

--- sources/memory/tag_directory/tag_directory.testbench.sv
+++ sources/memory/tag_directory/tag_directory.testbench.sv
@@ -29,11 +29,13 @@ localparam int  WIDTH_POW2   = 2**WIDTH;
 localparam int  INDEX_WIDTH  = $clog2(DEPTH);
 
 // Check parameters
-localparam int  SIMULTANEOUS_CHECK_DURATION    = 100;
-localparam int  RANDOM_CHECK_DURATION          = 100;
-localparam real RANDOM_CHECK_WRITE_PROBABILITY = 0.5;
-localparam real RANDOM_CHECK_READ_PROBABILITY  = 0.5;
-localparam int  RANDOM_CHECK_TIMEOUT           = 1000;
+localparam int  SIMULTANEOUS_CHECK_DURATION       = 100;
+localparam int  SIMULTANEOUS_CHECK_TIMEOUT        = 1000;
+localparam int  RANDOM_CHECK_DURATION             = 100;
+localparam real RANDOM_CHECK_ALLOCATE_PROBABILITY = 0.5;
+localparam real RANDOM_CHECK_SEARCH_PROBABILITY   = 0.5;
+localparam real RANDOM_CHECK_EVICT_PROBABILITY    = 0.3;
+localparam int  RANDOM_CHECK_TIMEOUT              = 1000;
 
 // Device ports
 logic                   clock;
@@ -50,11 +52,16 @@ logic                   evict_enable;
 logic [INDEX_WIDTH-1:0] evict_index;
 
 // Test variables
+int                     check;
 logic [INDEX_WIDTH-1:0] last_index;
 logic       [DEPTH-1:0] model_valid;
 logic       [WIDTH-1:0] model_tags [DEPTH-1:0];
 logic                   expected_hit;
 logic [INDEX_WIDTH-1:0] expected_index;
+int                     search_random_valid_bit;
+int                     evict_random_valid_bit;
+int                     transfer_count;
+int                     timeout_countdown;
 
 // Allocate
 task automatic allocate;
@@ -81,7 +88,7 @@ task automatic evict(input [INDEX_WIDTH-1:0] index_to_evict);
 endtask
 
 // Update the expected search results based on the model
-task automatic search_for_tag(input [WIDTH-1:0] tag_to_search);
+task automatic update_search_expected(input [WIDTH-1:0] tag_to_search);
   expected_hit   = 0;
   expected_index = 'x;
 `ifndef SIMULATOR_NO_BREAK
@@ -99,21 +106,28 @@ task automatic search_for_tag(input [WIDTH-1:0] tag_to_search);
   end
 endtask
 
+// Search
+task automatic search(input [WIDTH-1:0] tag_to_search);
+  search_tag = tag_to_search;
+  update_search_expected(search_tag);
+  @(posedge clock);
+  // Check DUT
+  if (expected_hit) begin
+    assert (search_hit) else $error("[%0tns] Search miss for allocated tag '0x%0h'.", $time, search_tag);
+    assert (search_index == expected_index) else $error("[%0tns] Index of searched tag is incorrect (expected '0x%0d', got '0x%0d').", $time, expected_index, search_index);
+  end else begin
+    assert (!search_hit) else $error("[%0tns] Search hit on unallocated tag '0x%0h' at index '%0d'.", $time, search_tag, search_index);
+  end
+  @(negedge clock);
+  search_tag = 0;
+endtask
+
 // Search all tags and compare with the model
 task automatic search_all;
   for (int tag = 0; tag < WIDTH_POW2; tag++) begin
     // Search
     @(negedge clock);
-    search_tag = tag;
-    search_for_tag(search_tag);
-    @(posedge clock);
-    // Check DUT
-    if (expected_hit) begin
-      assert (search_hit) else $error("[%0tns] Search miss for allocated tag '0x%0h'.", $time, search_tag);
-      assert (search_index == expected_index) else $error("[%0tns] Index of searched tag is incorrect (expected '0x%0d', got '0x%0d').", $time, expected_index, search_index);
-    end else begin
-      assert (!search_hit) else $error("[%0tns] Search hit on unallocated tag '0x%0h' at index '%0d'.", $time, search_tag, search_index);
-    end
+    search(tag);
   end
 endtask
 
@@ -175,7 +189,7 @@ initial begin
   @(posedge clock);
 
   // Check 1 : Allocate once
-  $display("CHECK 1 : Allocate once.");
+  $display("CHECK 1 : Allocate once."); check = 1;
   // Initial state
   check_flags(false, true, " after reset");
   // Allocate
@@ -189,7 +203,7 @@ initial begin
   repeat(10) @(posedge clock);
 
   // Check 2 : Evict once
-  $display("CHECK 2 : Evict once.");
+  $display("CHECK 2 : Evict once."); check = 2;
   // Evict
   @(negedge clock);
   evict(last_index);
@@ -201,7 +215,7 @@ initial begin
   repeat(10) @(posedge clock);
 
   // Check 3 : Allocate all
-  $display("CHECK 3 : Allocate all.");
+  $display("CHECK 3 : Allocate all."); check = 3;
   repeat (DEPTH) begin
     // Allocate
     @(negedge clock);
@@ -215,13 +229,137 @@ initial begin
   repeat(10) @(posedge clock);
 
   // Check 4 : Evict all
-  $display("CHECK 4 : Evict all.");
+  $display("CHECK 4 : Evict all."); check = 4;
   for (int index = 0; index < DEPTH; index++) begin
     // Evict
     evict(index);
   end
   // Final state
   check_flags(false, true, " after evicting all. The directory should be empty.");
+  // Search all
+  search_all();
+
+  repeat(10) @(posedge clock);
+
+  // Check 5 : Simultaneous operations
+  $display("CHECK 5 : Simultaneous operations."); check = 5;
+  @(negedge clock);
+  transfer_count    = 0;
+  timeout_countdown = SIMULTANEOUS_CHECK_TIMEOUT;
+  fork
+    // Allocating
+    begin
+      forever begin
+        @(negedge clock);
+        if (!full && transfer_count < SIMULTANEOUS_CHECK_DURATION) begin
+          allocate();
+          transfer_count++;
+        end
+      end
+    end
+    // Searching
+    begin
+      forever begin
+        @(negedge clock);
+        search_random_valid_bit = random_in_mask(model_valid);
+        if (search_random_valid_bit != -1) begin
+          search(model_tags[search_random_valid_bit]);
+        end
+      end
+    end
+    // Evicting
+    begin
+      forever begin
+        @(negedge clock);
+        evict_random_valid_bit = random_in_mask(model_valid);
+        if (evict_random_valid_bit != -1) begin
+          evict(evict_random_valid_bit);
+        end
+      end
+    end
+    // Stop condition
+    begin
+      // Transfer count
+      while (transfer_count < SIMULTANEOUS_CHECK_DURATION) begin
+        @(negedge clock);
+      end
+      // Evict until empty
+      while (!empty) begin
+        @(negedge clock);
+      end
+    end
+    // Timeout
+    begin
+      while (timeout_countdown > 0) begin
+        @(negedge clock);
+        timeout_countdown--;
+      end
+      $error("[%0tns] Timeout.", $time);
+    end
+  join_any
+  disable fork;
+  // Search all
+  search_all();
+
+  repeat(10) @(posedge clock);
+
+  // Check 6 : Random stimulus
+  $display("CHECK 6 : Random stimulus."); check = 6;
+  @(negedge clock);
+  transfer_count    = 0;
+  timeout_countdown = RANDOM_CHECK_TIMEOUT;
+  fork
+    // Allocating
+    begin
+      forever begin
+        @(negedge clock);
+        if (!full && random_boolean(RANDOM_CHECK_ALLOCATE_PROBABILITY) && transfer_count < RANDOM_CHECK_DURATION) begin
+          allocate();
+          transfer_count++;
+        end
+      end
+    end
+    // Searching
+    begin
+      forever begin
+        @(negedge clock);
+        search_random_valid_bit = random_in_mask(model_valid);
+        if (search_random_valid_bit != -1 && random_boolean(RANDOM_CHECK_SEARCH_PROBABILITY)) begin
+          search(model_tags[search_random_valid_bit]);
+        end
+      end
+    end
+    // Evicting
+    begin
+      forever begin
+        @(negedge clock);
+        evict_random_valid_bit = random_in_mask(model_valid);
+        if (evict_random_valid_bit != -1 && random_boolean(RANDOM_CHECK_EVICT_PROBABILITY)) begin
+          evict(evict_random_valid_bit);
+        end
+      end
+    end
+    // Stop condition
+    begin
+      // Transfer count
+      while (transfer_count < RANDOM_CHECK_DURATION) begin
+        @(negedge clock);
+      end
+      // Evict until empty
+      while (!empty) begin
+        @(negedge clock);
+      end
+    end
+    // Timeout
+    begin
+      while (timeout_countdown > 0) begin
+        @(negedge clock);
+        timeout_countdown--;
+      end
+      $error("[%0tns] Timeout.", $time);
+    end
+  join_any
+  disable fork;
   // Search all
   search_all();
 
